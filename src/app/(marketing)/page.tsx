02aa0121f53efc1f2bfe780b3a4b1afd8ce1477c
Chidera paul ogbu <ogbupaul545@gmail.com>
2026-05-09 08:52:07 +0100

    feature: implement testionial feature on landing page with carousel

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,7 +1,8 @@
+import Testimonials from './components/testimonials';
 export default function LandingPage() {
   return (
     <div className="max-w-360 mx-auto py-2 px-4 sm:px-6 lg:px-8">
-      Replace this paragraph with your section imports
+      <Testimonials />
     </div>
   );
 }
