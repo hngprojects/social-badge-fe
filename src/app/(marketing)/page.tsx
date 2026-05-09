e280d8a0602fa0ca27a640867c4cc18d5bc2e4df
truella <turahsuleiman1212@gmail.com>
2026-05-09 10:39:41 +0100

    chore: remove local testing changes from page.tsx

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,7 @@
-import TryItNow from './components/TryItNow';
-
 export default function LandingPage() {
   return (
     <div className="max-w-360 mx-auto py-2 px-4 sm:px-6 lg:px-8">
-      <TryItNow />
+      Replace this paragraph with your section imports
     </div>
   );
 }
