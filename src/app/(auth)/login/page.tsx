47cadaa4f648fc2945b2313229c1ec7a3f551d90
cynthia <abahojomacynthia@gmail.com>
2026-05-09 20:33:30 +0100

    feat(auth-flow): implement auth screen UIs

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,65 @@
+import { AuthInput } from '@/app/features/auth/components/auth-input';
+import { GoogleAuth } from '@/app/features/auth/components/google-auth';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+const page = () => {
+  return (
+    <div className="h-full flex flex-col justify-center gap-7">
+      <div className="page-info sm:leading-20">
+        <h1 className="page-title font-semibold text-[40px] sm:text-[52px] ">Login</h1>
+        <p className="font-medium text-base sm:text-[18px] text-[#978B8A]">
+          Welcome back to your account
+        </p>
+      </div>
+
+      <div className="login-form bg-[#F5F5F5] rounded-lg px-5 py-6 flex flex-col gap-7">
+        <form className=" flex flex-col gap-5">
+          <div className="flex flex-col gap-4">
+            <AuthInput type="email" placeholder="Enter your email" label={'Email'} />
+
+            <div className="">
+              <AuthInput type="password" placeholder="Enter your password" label={'Password'} />
+              <div className="flex items-center justify-between flex-wrap-reverse gap-2 mt-2">
+                <div className="flex items-center gap-2">
+                  <input
+                    type="checkbox"
+                    className="h-6 w-6 rounded-[5px] border border-[#727272] accent-[#FA5424]"
+                    name="remember-me"
+                    id="remember-me"
+                  />
+                  <label htmlFor="remember-me" className="text-sm text-[#978B8A]">
+                    Remember me
+                  </label>
+                </div>
+
+                <Link
+                  href={'/forgot-password'}
+                  className="text-sm text-[#978B8A] hover:filter hover:brightness-70"
+                >
+                  Forgot password?
+                </Link>
+              </div>
+            </div>
+          </div>
+
+          <Button type="submit" name="Login" />
+        </form>
+
+        {/* google auth */}
+        <GoogleAuth />
+
+        <div className="text-center">
+          <p className="text-md">
+            Don&apos;t have an account?{' '}
+            <a href="/signup" className="font-bold text-[#FA5424] hover:text-[#e14b1c]">
+              Sign up
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/features/auth/types.ts
@@ -0,0 +1,7 @@
+export type UserAuth = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
